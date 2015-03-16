fa1aa1f670fae903cf9914e8abf26a931a47e6b6
yanzhe19 <yanzhe19@gmail.com>
2015-03-16 13:22:20 -0400

    added new route for daniel

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node Game' });
 });
 
+/* GET Daniel page. */
+router.get('/daniel', function(req, res, next) {
+  res.render('index', { title: 'Node Game Daniel' });
+});
+
+
 module.exports = router;
